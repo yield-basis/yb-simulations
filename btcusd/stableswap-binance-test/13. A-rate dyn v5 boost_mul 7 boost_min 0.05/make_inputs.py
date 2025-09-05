9fb1b9f30fb113cedff8adc69e0f96c7f77202c9
Michael Egorov <michwill@gmail.com>
2025-09-06 00:41:30 +0200

    Testing with Binance data

---
+++ btcusd/stableswap-binance-test/13. A-rate dyn v5 boost_mul 7 boost_min 0.05/make_inputs.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(1), log10(50), 64)
+Xname = "A"
+Y = np.logspace(log10(0.05), log10(5), 64)
+Yname = "boost_rate"
+
+other_params = dict(
+    D=20e6,
+    adjustment_step=1e-7,
+    fee_gamma=0.314,
+    ma_half_time=600,
+    out_fee=0.0146,
+    mid_fee=0.005,
+    gas_fee=1,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.00005,
+    gamma=0,
+    boost_rate=0.05,
+    boost_min=0.05,
+    boost_mul=7,
+    A=10)
+
+config = {
+    'configuration': [],
+    'datafile': ["btcusdt-processed-1y-2024-2025"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
---
+++ btcusd/stableswap-binance-test/6. scan-dyn v1/make_inputs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(0.03), log10(0.2), 64)
+Xname = "boost_rate"
+Y = np.logspace(log10(400), log10(10**6), 64)
+Yname = "boost_mul"
+
+other_params = dict(
+    D=20e6,
+    adjustment_step=1e-7,
+    fee_gamma=0.314,
+    ma_half_time=600,
+    out_fee=0.0146,
+    mid_fee=0.005,
+    gas_fee=1,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.00005,
+    gamma=0,
+    boost_rate=0.05,
+    boost_mul=10,
+    A=7.2)
+
+config = {
+    'configuration': [],
+    'datafile': ["btcusdt-processed-1y-2024-2025"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
