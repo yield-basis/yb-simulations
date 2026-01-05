613583da9a37575b6ff6563defb74332a7756248
Michael Egorov <michwill@gmail.com>
2026-01-05 21:06:01 +0100

    More BTC simulations

---
+++ btcusd/Jan2025-Jan2026/2. Hierarchical scans/A13/of-1.9/mf-0.46/make_inputs.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(0.002), log10(0.20), 100)
+Xname = "boost_rate"
+Y = np.logspace(log10(1e-4), log10(0.1), 100)
+Yname = "fee_gamma"
+
+other_params = dict(
+    D=20e6,
+    adjustment_step=1e-7,
+    fee_gamma=0.003,
+    ma_half_time=600,
+    mid_fee=0.0046,
+    out_fee=0.019,
+    gas_fee=1,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0004,
+    gamma=0,
+    boost_rate=0.046,
+    A=13)
+
+config = {
+    'configuration': [],
+    'datafile': ["btcusdt-J2025-J2026"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
---
+++ btcusd/Sep2025-Jan2026/1. A-mid_fee/make_inputs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(2), log10(50), 100)
+Xname = "A"
+Y = np.logspace(log10(10e-4), log10(0.1), 100)
+Yname = "mid_fee"
+
+other_params = dict(
+    D=20e6,
+    adjustment_step=1e-7,
+    fee_gamma=0.003,
+    ma_half_time=600,
+    mid_fee=0.003,
+    out_fee=0.003,
+    gas_fee=1,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0004,
+    gamma=0,
+    boost_rate=0.035,
+    A=9)
+
+config = {
+    'configuration': [],
+    'datafile': ["btcusdt-Sep2025-Jan2026"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    params['out_fee'] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
