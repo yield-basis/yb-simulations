1d3d61b273475d2ca29c0eb34bc7ea1f202d8235
Michael Egorov <michwill@gmail.com>
2026-02-22 17:43:57 +0100

    Using cumulative product in the graph

--- btcusd/dyn calc/6. releverage A9 rate6/plot_xcp.py
+++ btcusd/dyn calc/6. releverage A9 rate6/plot_xcp.py
@@ -17,7 +17,7 @@ with lzma.open('detailed-output.json.xz', 'r') as f:
 t = np.array([d['t'] for d in data])
 vp = np.array([(d['xcp'] + 1) / 2 for d in data])
 ri = np.array([d['boost_rate'] for d in data])
-ri = 1 + np.array([0] + list(np.cumsum(ri[:-1] * (t[1:] - t[:-1]))))
+ri = [1] + list(np.cumprod(1 + ri[:-1] * (t[1:] - t[:-1])))
 
 t = [datetime.fromtimestamp(_t) for _t in t[::STEP]]
 vp = vp[::STEP] / ri[::STEP]
