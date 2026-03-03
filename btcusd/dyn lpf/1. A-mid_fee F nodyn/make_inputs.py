3583471d34da99c4a0fd62a7dca833fe1845a760
Michael Egorov <michwill@gmail.com>
2026-03-03 10:46:42 +0100

    More sims with changing the fraction for LP to other than 50\%

---
+++ btcusd/dyn lpf/1. A-mid_fee F nodyn/make_inputs.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(2), log10(50), 32)
+Xname = "A"
+Y = np.logspace(log10(10e-4), log10(0.1), 32)
+Yname = "mid_fee"
+
+other_params = dict(
+    D=20e6,
+    adjustment_step=5e-3,
+    fee_gamma=0.003,
+    ma_half_time=600,
+    mid_fee=0.003,
+    out_fee=0.003,
+    gas_fee=1,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0004,
+    gamma=0,
+    boost_rate=0.02,
+    boost_mul=1,
+    boost_min=0,
+    lp_profit_fraction=0.2,
+    A=9)
+
+config = {
+    'configuration': [],
+    'datafile': ["btcusdt-2024-F2026"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    params['out_fee'] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
