be5a1ab074a06b55f2c5172eb8810049096a2e82
Michael Egorov <michwill@gmail.com>
2026-03-17 17:40:52 +0100

    More sims at low As

---
+++ btcusd/lpf sliced/train-1747522800000/2. rate-fg, A9/make_inputs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(0.003), log10(0.055), 100)
+Xname = "boost_rate"
+Y = np.logspace(log10(1e-6), log10(1), 100)
+Yname = "fee_gamma"
+
+other_params = dict(
+    D=20e6,
+    adjustment_step=5e-3,
+    fee_gamma=0.003,
+    ma_half_time=600,
+    mid_fee=0.00375,
+    out_fee=0.02,
+    gas_fee=0,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0002,
+    gamma=0,
+    boost_rate=0.035,
+    A=9,
+    lp_profit_fraction=0.3,
+    boost_mul=1,
+    boost_min=0)
+
+config = {
+    'configuration': [],
+    'datafile': ["train-b-1747522800000-btcusd"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
