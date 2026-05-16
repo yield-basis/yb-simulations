296fb39517691f94a7d54460559e136cfb2ddbec
Michael Egorov <michwill@gmail.com>
2026-05-16 12:08:25 +0200

    Ethereum update

---
+++ ethusd/lpf/6. of-fg, A5, mf 0.5/make_inputs.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from math import log10
+import json
+import itertools
+from copy import copy
+
+
+X = np.logspace(log10(0.0082), log10(0.05), 100)
+Xname = "out_fee"
+Y = np.logspace(log10(1e-5), log10(1e-1), 100)
+Yname = "fee_gamma"
+
+other_params = dict(
+    D=20e6,
+    adjustment_step=5e-3,
+    fee_gamma=0.00622,
+    ma_half_time=600,
+    mid_fee=0.005,
+    out_fee=0.015,
+    gas_fee=0,
+    n=2,
+    log=0,
+    allowed_extra_profit=1e-10,
+    ext_fee=0.0002,
+    gamma=0,
+    boost_rate=0.0512,
+    A=5,
+    lp_profit_fraction=0.4284,
+    boost_mul=1,
+    boost_min=0)
+
+config = {
+    'configuration': [],
+    'datafile': ["ethusdt-2024-May2026"],
+    'debug': 0}
+
+for x, y in itertools.product(X, Y):
+    params = copy(other_params)
+    params[Xname] = x
+    params[Yname] = y
+    config['configuration'].append(params)
+
+with open('configuration.json', 'w') as f:
+    json.dump(config, f)
